9d8f6cc2f5b9a202ce70e0c6892c2b780748bdb0
Kyle <kyle.rozanitis@gmail.com>
2019-02-14 17:25:14 -0500

    added function to check for balance of more symbol types

--- BalParChecker.py
+++ BalParChecker.py
@@ -3,10 +3,10 @@ is balanced using a stack. """
 
 from pythonds.basic.stack import Stack
 
-def balParChecker(symbolString):
+def bal_par_checker(symbol_string):
     s = Stack()
 
-    for item in symbolString:
+    for item in symbol_string:
         if item == "(":
             s.push(item)
         elif item == ")":
@@ -19,7 +19,43 @@ def balParChecker(symbolString):
         
     return True if s.isEmpty() else False
 
-print(balParChecker('((()))'))
-print(balParChecker('((())))'))
-print(balParChecker('((())'))
-print(balParChecker('(((x)))'))
+
+def bal_symbol_checker(symbol_string):
+    s = Stack()
+
+    openers = "({["
+    closers = ")}]"
+
+    for item in symbol_string:
+        if item in openers:
+            s.push(item)
+        elif item in closers:
+            if s.isEmpty():
+                return False
+            else:
+                top = s.pop()
+                if not matcher(top, item):
+                    return False
+        else:
+            return "Invalid input. Input must only contain opening and closing parenthses."
+    
+    return True if s.isEmpty() else False
+
+
+def matcher(opener, closer):
+    openers = "({["
+    closers = ")}]"
+
+    return openers.index(opener) == closers.index(closer)
+
+
+print(bal_par_checker('((()))'))
+print(bal_par_checker('((())))'))
+print(bal_par_checker('((())'))
+print(bal_par_checker('(((x)))'))
+
+print(bal_symbol_checker('({[]})'))
+print(bal_symbol_checker('({[}])'))
+print(bal_symbol_checker('({[[]]]})'))
+print(bal_symbol_checker('({[]}))'))
+print(bal_symbol_checker('({[]x})'))
